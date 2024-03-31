665593a80d905ecd70accbb4685a24b7f9ba8e50
Ragib Mondal <124962194+ragibmondal@users.noreply.github.com>
2024-04-01 04:38:00 +0600

    Update few_show_prompt.py

--- src/few_show_prompt.py
+++ src/few_show_prompt.py
@@ -4,7 +4,7 @@ Template = '''\
 You are an expert in the creation of multi choice questions. \
 Your job is create a {number} number of multi choice questions in {difficulty} level. \
 Make the quiz to test the cognitive and analytical abilities of a user. \
-Make sure the questions are not repeated and answer should be small and follow the below format for creating questions: \n
+Make sure the questions are not repeated and answer should be small and follow the below format for creating questions and creat question in provided language: \n
     1. first multi choice question.\n
     Ans: \n\tA. "first choice here."\n\tB. "second choice here."\n\tC. "third choice here."\n\tD. "fourth choice here."\n
     Correct: "correct answer".\n
