594a4c2fbfb57d9d0753d40def9cf65bc283d149
Ragib Mondal <124962194+ragibmondal@users.noreply.github.com>
2024-04-01 05:27:27 +0600

    Update data_util.py

--- src/data_util.py
+++ src/data_util.py
@@ -1,27 +1,35 @@
+
 import PyPDF2
+from io import BytesIO
+
 from src.logger import logging
 
 # Reading the input data file
 def read_input_file(file_name):
     # checking the file type
-    if file_name.name.endswith(".pdf"):
+    if file_name.type == "application/pdf":
         # Input file is a PDF
         try:
             # Reading the PDF file
-            pdf_reader = PyPDF2.PdfFileReader(file_name)
+            pdf_reader = PyPDF2.PdfReader(BytesIO(file_name.read()))
             text = ""
             # Extracting text from each page and adding to text
-            for page in pdf_reader.pages:
+            for page_num in range(len(pdf_reader.pages)):
+                page = pdf_reader.pages[page_num]
                 text += page.extract_text()
-            del pdf_reader
-            logging.info('Input PDF file was raed')
+
+            logging.info('Input PDF file was read')
             return text
-        except:
+
+        except Exception as e:
+            logging.error(f"Error while reading input PDF file: {e}")
             raise Exception("Error while reading input PDF file.")
-    elif file_name.name.endswith(".txt"):
+
+    elif file_name.type == "text/plain":
         # Input file is a text
-        logging.info('Input Text file was raed')
+        logging.info('Input Text file was read')
         return file_name.read().decode('utf-8')
+
     else:
         # raise exception
         raise Exception("Unsupported file format. Only PDF and Text files are supported.")
